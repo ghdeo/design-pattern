320cc2e3612369003fcfa25df0d02d3f33ff955f
ghdeo <goldencorn7@naver.com>
2023-09-10 23:47:03 +0900

    feat: Add singleton code comments

--- singleton/source/code/spring-src/main/java/com/example/practice/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.repository;
-
-import com.example.practice.domain.Member;
-
-public interface MemberRepository {
-    void save(Member member);
-
-    Member findById(Long memberId);
-}
